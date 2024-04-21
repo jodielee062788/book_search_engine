7f86fbc281fc2515dc4a1d1c848cd4b53c0cd518
Jodie <jodielee062788@gmail.com>
2024-04-21 21:17:12 +1000

    Define the query and mutation functionality

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password');
+          return userData;
+        }
+        throw new Error('You need to log in first!');
+    },
+},
+    Mutation: {
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("This user can't be found");
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+          throw new Error('Incorrect Password!');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
+      },
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+            
+        if (!user) {
+          throw new Error("Can't create user!");
+        }
+            
+        const token = signToken(user);
+        return { token, user };
+      },
+      saveBook: async (parent, { book }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { savedBooks: book } },
+            { new: true, runValidators: true }
+          );
+              
+          return updatedUser;
+        }
+          throw new Error('You need to log in first!');
+      },
+      removeBook: async (_, { bookId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId } } },
+            { new: true }
+          );
+              
+          if (!updatedUser) {
+            throw new Error("This user can't be found");
+          }
+              
+          return updatedUser;
+        }
+          throw new Error('You need to log in first!');
+       },    
+    },
+};
+      
+module.exports = resolvers;
