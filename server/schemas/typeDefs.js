ceb5896c7d208bbfd3429ce27c29679c66b98c3f
Jodie <jodielee062788@gmail.com>
2024-04-23 20:21:04 +1000

    Fix typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,44 +1,46 @@
-// Define the necessary `Query` and `Mutation` types
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
-  type Book {
-    bookId: ID
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-  }
   input BookInput {
     authors: [String]
     description: String
+    title: String!
     bookId: String!
     image: String
     link: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
     title: String!
+    image: String
+    link: String
   }
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookInput: BookInput): User
-    removeBook(bookId: ID!): User
-  }
+
   type Auth {
     token: ID!
     user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: String!): User
   }
 `;
 
